9accb80bb7442004d54862a7dbe87d75d00ef8c5
Vitalii <72293599+VitaliiDan@users.noreply.github.com>
2021-05-11 11:25:37 +0200

    finish app

---
+++ src/components/Form.js
@@ -0,0 +1,40 @@
+import React, {useState, useContext} from "react";
+import {AlertContext} from "../context/alert/alertContext";
+import {FirebaseContext} from "../context/firebase/firebaseContext";
+
+export const Form = () => {
+    const [value, setValue] = useState('');
+    const alert = useContext(AlertContext);
+    const firebase = useContext(FirebaseContext);
+
+    const submitHandler = event => {
+        event.preventDefault();
+        if (value.trim()) {
+            firebase.addNote(value.trim())
+                .then(() => {
+                    alert.show('note is add', 'success');
+                })
+                .catch(() => {
+                    alert.show('Something went wrong', 'danger');
+                });
+
+            setValue('');
+        } else {
+            alert.show('enter text')
+        }
+    }
+
+    return (
+        <form onSubmit={submitHandler}>
+            <div className="form-group">
+                <input
+                    type="text"
+                    className='form-control'
+                    placeholder='Enter note-name'
+                    value={value}
+                    onChange={event => setValue(event.target.value)}
+                />
+            </div>
+        </form>
+    )
+}
---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const About = () => (
+        <div className='jumbotron'>
+            <div className='container'>
+                <h1 className='display-4'> BEST APP </h1>
+                <p className='lead'> APP version <strong>1.1.1</strong></p>
+            </div>
+        </div>
+    )
